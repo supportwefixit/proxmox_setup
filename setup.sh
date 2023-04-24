62b76dc79f6ac0b9c263a1e5b793346d6dc677bd
We Fix IT <support@wefixit.com.au>
2023-04-24 15:12:08 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,17 +7,6 @@ read -p "Enter the new gateway of the server (10.10.10.1): " new_gateway
 read -p "Enter the new hostname of the server (pve1): " new_hostname
 read -p "Enter the new domain for the server (example.com): " new_domain
 
-# Update /etc/network/interfaces
-sed -i "/^\\taddress /s/\\(.* \\).*/\\1$new_ip/" /etc/network/interfaces
-sed -i "/^\\tnetmask /s/\\(.* \\).*/\\1$new_netmask/" /etc/network/interfaces
-sed -i "/^\\tgateway /s/\\(.* \\).*/\\1$new_gateway/" /etc/network/interfaces
-
-# Update /etc/hosts
-sed -i "2s/.*/$new_ip\t$new_hostname.$new_domain\t$new_hostname/" /etc/hosts
-
-# Update /etc/hostname
-echo "$new_hostname" > /etc/hostname
-
 # Rewrite /etc/apt/sources.list file
 echo "Rewriting /etc/apt/sources.list... "
 echo "deb http://ftp.au.debian.org/debian bullseye main contrib
@@ -27,21 +16,9 @@ deb http://security.debian.org bullseye-security main contrib
 deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list
 echo "Complete"
 
-
 # Rewrite /etc/apt/sources.list.d/pve-enterprise.list file
 echo "Rewriting enterprise sources list... "
 echo "#deb https://enterprise.proxmox.com/debian/pve bullseye pve-enterprise" > /etc/apt/sources.list.d/pve-enterprise.list
-echo "Complete"
-
-# Restart network interfaces
-echo "Restarting network interfaces... "
-ifdown vmbr0
-ifup vmbr0
-echo "Complete"
-
-# Restart network services
-echo "Restarting network services... "
-systemctl restart networking
 echo "Complete"
 
 # Prompt for Updates
@@ -55,6 +32,11 @@ else
   echo "Updates skipped... Moving on"
 fi
 
+# Update CT Template list
+echo "Updating CT Template list... "
+pveam update > /dev/null
+echo "Complete"
+
 # Remove Proxmox Subscription nag
 echo "Removing Proxmox Subscription nag - METHOD 1... "
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
@@ -66,10 +48,37 @@ wget -q -O rem_proxmox_popup.sh 'https://gist.github.com/tavinus/08a63e7269e0f70
 ./rem_proxmox_popup.sh
 echo "Complete"
 
-# Update CT Template list
-echo "Updating CT Template list... "
-pveam update > /dev/null
-echo "Complete"
+# Update /etc/network/interfaces
+sed -i "/^\\taddress /s/\\(.* \\).*/\\1$new_ip/" /etc/network/interfaces
+sed -i "/^\\tnetmask /s/\\(.* \\).*/\\1$new_netmask/" /etc/network/interfaces
+sed -i "/^\\tgateway /s/\\(.* \\).*/\\1$new_gateway/" /etc/network/interfaces
+
+# Update /etc/hosts
+sed -i "2s/.*/$new_ip\t$new_hostname.$new_domain\t$new_hostname/" /etc/hosts
+
+# Update /etc/hostname
+echo "$new_hostname" > /etc/hostname
+
+# Prompt to Restart network interfaces
+read -p "Do you want to restart network interfaces now (y/n)? " nic_answer
+if [[ $nic_answer =~ ^[Yy]$ ]]; then
+  echo "Restarting network interfaces. Please wait..."
+  ifdown vmbr0 > /dev/null
+  ifup vmbr0 > /dev/null
+  echo "Restart Complete"
+else
+  echo "Skipped... Moving on"
+fi
+
+# Prompt to Restart network services
+read -p "Do you want to restart network services now (y/n)? " nicservices_answer
+if [[ $nicservices_answer_answer =~ ^[Yy]$ ]]; then
+  echo "Restarting network services. Please wait..."
+  systemctl restart networking > /dev/null
+  echo "Restart Complete"
+else
+  echo "Skipped... Moving on"
+fi
 
 # Prompt for reboot
 read -p "Do you want to reboot the server now (y/n)? " reboot_answer
